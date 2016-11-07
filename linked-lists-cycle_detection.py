0e0a3a2dfbe3f3139f75e9eb8bf207042f8ec167
Jess Stewart <jeselistewart@gmail.com>
2016-11-07 08:23:20 -0800

    Update linked-lists-cycle_detection.py

--- linked-lists-cycle_detection.py
+++ linked-lists-cycle_detection.py
@@ -1,1 +1,35 @@
+class Node(object):
+    def __init__(self, data = None, next_node = None):
+        self.data = data
+        self.next = next_node
 
+
+def has_cycle(head):
+    visited_nodes = []
+    if head.data == None:
+        return False
+
+    visited_nodes.append(head)
+
+    current = head.next
+    while current:
+        if current in visited_nodes:
+            return True
+        else:
+            visited_nodes.append(current)
+            current = current.next
+
+    return False
+
+
+case_one = Node(1)
+case_two = Node(1)
+case_two_2 = Node(2)
+case_two_3 = Node(3)
+
+case_two_3.next = case_two_2
+case_two_2.next = case_two_3
+case_two.next = case_two_2
+
+print(has_cycle(case_one))
+print(has_cycle(case_two))
