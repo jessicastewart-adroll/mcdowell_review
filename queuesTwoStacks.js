51cdb97444a67c074d79e40c46452eb4b30496e0
Jess Stewart <jeselistewart@gmail.com>
2016-11-10 06:57:47 -0800

    Create queuesTwoStacks.js

---
+++ queuesTwoStacks.js
@@ -0,0 +1,11 @@
+'''
+enqueue: add element to end
+dequeue: remove element from front
+
+*create queue using 2 stacks
+  1 enqueue
+  2 dequeue
+  3 print first
+  
+*input: line count, operation number, element
+'''
