d7aec02b5bdaf0875986418f36b97b93f2035478
Jess Stewart <jeselistewart@gmail.com>
2016-11-15 17:09:38 -0800

    Update is_binary_search_tree.py

--- is_binary_search_tree.py
+++ is_binary_search_tree.py
@@ -13,6 +13,11 @@ greather than on RIGHT
 input: root node
 output: True/False
 """
+class node:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = None
+        self.right = None
 
 def check_binary_search_tree(root):
     return check(root, float('inf'), float('-inf'))    
@@ -25,3 +30,20 @@ def check(node, pos_inf, neg_inf):
         return False
     
     return check(node.left) and check(node.right)
+
+
+one = Node(1)
+two = Node(2)
+three = Node(3)
+four = Node(4)
+five = Node(5)
+six = Node(6)
+seven = Node(7)
+
+root = four
+root.left = three
+root.right = five
+three.left = one
+three.right = two
+five.right = six
+six.right = seven
