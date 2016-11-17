407e9ed4b66742b2d634dad885fa92f9d82290ba
Jess Stewart <jeselistewart@gmail.com>
2016-11-17 09:06:24 -0800

    Update is_binary_search_tree.py

--- is_binary_search_tree.py
+++ is_binary_search_tree.py
@@ -5,13 +5,16 @@ class Node:
         self.right = None
 
 def check_binary_search_tree(root):
-    return check(root, float('inf'), float('-inf'))
+    return check(root)
 
-def check(node, pos_inf, neg_inf):
-    if not node:
+def check(node):
+    if not node or (not node.left and not node.right):
         return True
-    
-    if node.left > node.data or node.right < node.data:
+
+    if node.left and not node.left.data < node.data:
+        return False
+
+    if node.right and not node.right.data > node.data:
         return False
 
     return check(node.left) and check(node.right)
@@ -20,31 +23,30 @@ def check(node, pos_inf, neg_inf):
 one = Node(1)
 two = Node(2)
 three = Node(3)
+three_duplicate = Node(3)
 four = Node(4)
 five = Node(5)
 six = Node(6)
 seven = Node(7)
 
-binary_tree = four
-binary_tree.left = three
-binary_tree.right = five
-three.left = one
-three.right = two
-five.right = six
-six.right = seven
+'''
+binary_tree = three
+binary_tree.left = two
+binary_tree.right = four
 
 duplicate_tree = four
 duplicate_tree.left = three
 duplicate_tree.right = five
 three.left = one
-three.right = three
+three.right = three_duplicate
 five.right = six
 six.right = seven
+'''
 
-broken_tree = four
-broken_tree.left = three
-broken_tree.right = five
-three.left = one
-three.right = two
-five.left = six
-five.right = seven
+another_tree = four
+another_tree.left = two
+another_tree.right = six
+two.left = one
+two.right = three
+six.left = five
+six.right = seven
