d70102a16c900072c04532f2773ea9179fabe53c
Jess Stewart <jeselistewart@gmail.com>
2016-11-21 08:47:14 -0800

    Update heap_median.py

--- heap_median.py
+++ heap_median.py
@@ -1,1 +1,15 @@
+'''
+*sort increasing 
+*even number of inputs -> get average
 
+input -> integer
+output -> print integer rounded to 10th decimal 
+
+HEAP
+  min - root node smallest
+  max - root node largest
+  sorted top-to-bottom, left-to-right (complete tree)
+  insert: first available leaf then bubble up, if replace root then put at leaf and bubble up
+  implementation: NOT NODES, indexed arrays
+  
+'''
