d8cd683c83ef08b641926f65f9d4b72d4a1fade1
Jess Stewart <jeselistewart@gmail.com>
2016-11-21 08:59:45 -0800

    Update heap_median.py

--- heap_median.py
+++ heap_median.py
@@ -11,5 +11,18 @@ HEAP
   sorted top-to-bottom, left-to-right (complete tree)
   insert: first available leaf then bubble up, if replace root then put at leaf and bubble up
   implementation: NOT NODES, indexed arrays
-  
+    0 index is root
+    parent = (index-2)/2
+    left = (index * 2) + 1
+    right = (index * 2) + 2
+    *peek
+      returns first element (root)
+    *poll
+      returns first element (root) and removes
+    *add
+      add element to heap
+      add to root leaf
+      heapifyUp
+    *heapifyUp
+    *heapifyDown
 '''
