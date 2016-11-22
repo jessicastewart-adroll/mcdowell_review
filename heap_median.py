11168be420611d703ac776302b7094e807b22ead
Jess Stewart <jeselistewart@gmail.com>
2016-11-22 08:45:18 -0800

    Update heap_median.py

--- heap_median.py
+++ heap_median.py
@@ -26,3 +26,76 @@ HEAP
     *heapifyUp
     *heapifyDown
 '''
+class MinIntHeap(object):
+        def __init__(self):
+                self.capacity = 10
+                self.size = 0
+                self.items = []
+
+        def get_left_child_index(self, parent_index):
+                return (2*parent_index) + 1
+
+        def get_right_child_index(self, parent_index):
+                return (2*parent_index) + 2
+
+        def get_parent_index(self, child_index):
+                return (child_index - 1)/2
+
+        def has_left_child(self, index):
+                return get_left_child_index(index) < size
+
+        def has_right_child(self, index):
+                return get_right_child_index(index) < size
+
+        def has_parent(self, index):
+                return get_parent_index(index) >= 0
+
+        def left_child(self, index):
+                return items[get_left_child_index(index)]
+
+        def right_child(self, index):
+                return items[get_right_child_index(index)]
+
+        def parent(self, index):
+                return items[get_parent_index(index)]
+
+        def swap(index_one, index_two):
+                self.items[indexOne], self.items[indexTwo] = self.items[indexTwo], self.items[indexOne]
+
+        def peek(self):
+                if self.size == 0:
+                        raise Exception("Heap is empty.")
+                return self.items[0]
+
+        def poll(self):
+                if self.size == 0:
+                        raise Exception("Heap is empty.")
+
+                item = self.items[0]
+                self.items[0] = self.items[self.size - 1]
+                self.size -= 1
+                self.heapify_down()
+                return item
+
+        def add(item):
+                self.items[self.size] = item
+                self.size += 1
+                self.heapify_up()
+
+        def heapify_up(self):
+                index = self.size - 1
+                while self.has_parent(index) and self.parent(index) > self.items[index]:
+                        self.swap(self.get_parent_index(index), index)
+                        index = self.get_parent_index(index)
+
+        def heapify_down(self):
+                index = 0
+                while self.has_left_child(index):
+                        smaller_child_index = self.get_left_child_index(index)
+                        if self.has_right_child(index) and self.right_child(index) < self.left_child(index):
+                                smaller_child_index = self.get_right_child_index(index)
+                        if self.items < self.items[smaller_child_index]:
+                                break
+                        else:
+                                self.swap(index, smaller_child_index)
+                        index = smaller_child_index
