0d1213c94dee7ec92546ed60763ec16561ba2021
Jess Stewart <jeselistewart@gmail.com>
2016-11-28 07:51:45 -0800

    Update heap_median.py

--- heap_median.py
+++ heap_median.py
@@ -1,30 +1,6 @@
 '''
-*sort increasing 
-*even number of inputs -> get average
-
-input -> integer
-output -> print integer rounded to 10th decimal 
-
-HEAP
-  min - root node smallest
-  max - root node largest
-  sorted top-to-bottom, left-to-right (complete tree)
-  insert: first available leaf then bubble up, if replace root then put at leaf and bubble up
-  implementation: NOT NODES, indexed arrays
-    0 index is root
-    parent = (index-2)/2
-    left = (index * 2) + 1
-    right = (index * 2) + 2
-    *peek
-      returns first element (root)
-    *poll
-      returns first element (root) and removes
-    *add
-      add element to heap
-      add to root leaf
-      heapifyUp
-    *heapifyUp
-    *heapifyDown
+use data structure to access data at correct point
+http://www.geeksforgeeks.org/median-of-stream-of-integers-running-integers/
 '''
 class MinIntHeap(object):
         def __init__(self):
