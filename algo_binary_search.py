8c7ad725193ab99bded0e2c53f674c1b896a6c82
Jess Stewart <jeselistewart@gmail.com>
2017-02-18 12:08:59 -0800

    Update algo_binary_search.py

--- algo_binary_search.py
+++ algo_binary_search.py
@@ -1,31 +1,28 @@
-# print cost positions
-# index from 1
+def binary_search(a, left, right, to_find):
+    while (left <= right):
+        mid = left+(right-left)//2
+        if a[mid] == to_find:
+            return mid
+        elif a[mid] < to_find:
+            left = mid+1
+        else:
+            right = mid-1
+    
+    return -1
 
-def find_pair_position(pair, costs):
-    for i, cost in enumerate(costs):
+def get_flavors(flavors, flavor_count, money):
+    i = 0
+    while i < flavor_count and flavors[i] <= money: 
+        match = binary_search(flavors, i, flavor_count-1, money-flavors[i])
+        if match > -1:
+            print(i+1, flavors[i])
+            print(match+1, flavors[match])
+        i+=1  
 
 t = int(input().strip())
 for a0 in range(t):
-    fund = int(input().strip())
-    flavors = int(input().strip())
-    costs = list(map(int, input().strip().split(' ')))
-    
-    for i, cost in enumerate(costs):
-        if fund > cost:
-            pair_position = find_pair_position(fund-cost, costs)
-            if pair_position:
-                print(i+1, pair_position)
-
-def binary_search(values, left, right, to_find):
-  mid = len(values)//2
-
-  if values[mid] == to_find:
-    return mid
-  elif values[mid] < to_find:
-    return binary_search(values, to_find)
-  elif values[mid] > to_find:
-    return binary_search(values[:mid], to_find)
-
-  return -1
-	
-print(binary_search([1, 2, 3, 4, 5], 0, 4, 4))
+    money = int(input().strip())
+    n = int(input().strip())
+    a = list(map(int, input().strip().split(' ')))
+    print('new')
+    get_flavors(a, n, money)
