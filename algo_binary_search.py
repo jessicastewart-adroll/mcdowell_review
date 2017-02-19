bca948a14630873bf924f307d23ce557e5553ce9
Jess Stewart <jeselistewart@gmail.com>
2017-02-19 08:22:30 -0800

    Update algo_binary_search.py

--- algo_binary_search.py
+++ algo_binary_search.py
@@ -1,28 +1,53 @@
-def binary_search(a, left, right, to_find):
-    while (left <= right):
-        mid = left+(right-left)//2
-        if a[mid] == to_find:
-            return mid
-        elif a[mid] < to_find:
-            left = mid+1
-        else:
-            right = mid-1
+from functools import cmp_to_key
+
+class Flavor(object):
+    def __init__(self, position, cost):
+        self.position = position
+        self.cost = cost
+        
+    def __repr__(self):
+        return 'Flavor ${} at {}'.format(self.cost, self.position)
     
-    return -1
+    def comparator(a, b):
+        return a.cost - b.cost
+        
+def get_flavors(a):
+    flavors = []   
+    for i, c in enumerate(a):
+        flavors.append(Flavor(i+1, c))
+        
+    return flavors  
 
-def get_flavors(flavors, flavor_count, money):
-    i = 0
-    while i < flavor_count and flavors[i] <= money: 
-        match = binary_search(flavors, i, flavor_count-1, money-flavors[i])
-        if match > -1:
-            print(i+1, flavors[i])
-            print(match+1, flavors[match])
-        i+=1  
+def binary_search(array, left, right, value):
+    while left <= right:
+        mid = left+(right-left)//2
+        if array[mid].cost == value:
+            return array[mid].position
+        elif array[mid].cost > value:
+            right = mid - 1
+        elif array[mid].cost < value: 
+            left = mid + 1
+    
+    else:
+        return -1
 
+def get_flavor_pair(flavors, money):
+    for flavor in flavors:
+        cost_one = flavor.cost
+        cost_two = money-flavor.cost
+        if cost_two > 0:
+            result = binary_search(flavors, 0, len(flavors)-1, cost_two)
+            if result > -1:
+                print(flavor.position, result)
+                return
+        else:
+            return
+            
 t = int(input().strip())
 for a0 in range(t):
-    money = int(input().strip())
+    m = int(input().strip())
     n = int(input().strip())
     a = list(map(int, input().strip().split(' ')))
-    print('new')
-    get_flavors(a, n, money)
+    flavors = get_flavors(a) 
+    flavors = sorted(flavors, key=cmp_to_key(Flavor.comparator))
+    pair = get_flavor_pair(flavors, m)
