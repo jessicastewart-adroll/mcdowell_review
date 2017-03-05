13d2c685083202e6ff1a3197952b3c1c3ad9525b
Jess Stewart <jeselistewart@gmail.com>
2017-03-05 09:55:51 -0800

    Update algo_dfs.py

--- algo_dfs.py
+++ algo_dfs.py
@@ -1,21 +1,23 @@
 def get_max_region(grid, row_index, column_index):
-    print(row_index, column_index)
     row_bound = len(grid)
     column_bound = len(grid[0])
+    
     if grid[row_index][column_index] == 0:
         return 0
     
     if row_index >= 0 and column_index >= 0 and row_index < row_bound and column_index < column_bound:
         return 0
     
+    count = 1
+    grid[row_index][column_index] = 0
     neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     for neighbor in neighbors:
         row = row_index + neighbor[0]
         column = column_index + neighbor[1]
         # check bounds 
-        if row >= 0 and column >= 0 and row < row_bound and column < column_bound:
-            if grid[row][column] == 1:
-                return 1 + get_max_region(grid, row, column)
+        if row >= 0 and column >= 0 and row < row_bound and column < column_bound and grid[row][column] == 1:
+            count += 1
+    return count        
     
 
 def getBiggestRegion(grid):
@@ -26,3 +28,11 @@ def getBiggestRegion(grid):
             max_region = max(max_region, get_max_region(grid, row_index, column_index))
             return
             column_index += 1
+
+grid = [
+    [1, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [1, 0, 0, 0]
+]
+print(getBiggestRegion(grid))
