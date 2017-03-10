daecb139de1151c27aafb0d816d5bd4bcb54e8a1
Jess Stewart <jeselistewart@gmail.com>
2017-03-10 11:00:35 -0800

    Create algo_merge_sort.py

---
+++ algo_merge_sort.py
@@ -0,0 +1,27 @@
+def mergeCount(A, B, count=0):
+  C = []
+  while A and B:
+    if A[0] <= B[0]:
+      C.append(A[0])
+      A = A[1:]
+    else:
+      C.append(B[0])
+      B = B[1:]
+      count += 1
+      
+  if A:
+    C.extend(A)
+  if B:
+    C.extend(B)
+  return C, count
+  
+def merge_sort(input_list):
+  if len(input_list) == 1:
+    return input_list, 0
+  
+  mid = len(input_list)//2
+  left, count_left = merge_sort(input_list[:mid])
+  right, count_right = merge_sort(input_list[mid:])
+  return mergeCount(left, right, count_left + count_right)
+  
+print(merge_sort([100, 3, -1, 4, -7]))
