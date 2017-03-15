ee27706d413838680a3f9c4b9bf9e3f4a8689c12
Jess Stewart <jeselistewart@gmail.com>
2017-03-15 09:20:43 -0700

    Update ds_heap.py

--- ds_heap.py
+++ ds_heap.py
@@ -1,27 +1,40 @@
-array = []
-values = [12, 4, 5, 3, 8, 7]
-for value in values:
-    array.append(value)
-    print(running_median(array))
-    
 import heapq
 
-# max heap has smaller values 
-# min heap has larger values 
-# create max heap by using negative values
-
-# default is min heap 
-def running_median(array):
-  upper_min_heap = []
-  lower_max_heap = []
+def keep_even(array):
+  if not array:
+    return
+  if len(array) < 2:
+    return float(array[0])
+    
+  lower = []
+  upper = []
   for value in array:
-    if len(min_heap) > len(max_heap):
-      heapq.heappush(max_heap, value)
-    else if  
-
-
-array = []
-values = [12, 4, 5, 3, 8, 7]
-for value in values:
-    array.append(value)
-    print(running_median(array))
+    if len(lower) <= len(upper):
+      heapq.heappush(lower, -value)
+    else:
+      heapq.heappush(upper, value)
+    
+    if lower and upper:  
+      if -lower[0] > upper[0]:
+        first = -heapq.heappop(lower)
+        second = heapq.heappop(upper)
+        heapq.heappush(lower, -second)
+        heapq.heappush(upper, first)
+        
+  #print(lower, upper)      
+        
+  if len(lower) == len(upper):
+    first = -heapq.heappop(lower)
+    second = heapq.heappop(upper)
+    return float((first+second)/2)
+  elif len(lower) > len(upper): 
+    return float(-heapq.heappop(lower))
+  else: 
+    return float(heapq.heappop(upper))
+  
+print(keep_even([12]))
+print(keep_even([12, 4]))
+print(keep_even([12, 4, 5]))
+print(keep_even([12, 4, 5, 3]))
+print(keep_even([12, 4, 5, 3, 8]))
+print(keep_even([12, 4, 5, 3, 8, 7]))
