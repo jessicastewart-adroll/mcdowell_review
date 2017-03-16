a1a4e0f451b8688e49f7c830d69ba6e9001476ff
Jess Stewart <jeselistewart@gmail.com>
2017-03-16 09:12:25 -0700

    Update algo_dfs.py

--- algo_dfs.py
+++ algo_dfs.py
@@ -2,6 +2,38 @@ def largest_connected_component(board):
 	largest_count = 0
 	for row in range(len(board)):
 		for column in range(len(board[0])):
+			if board[row][column] == 1:
+				board[row][column] = 0
+				count = dfs(board, row, column, 1)
+				largest_count = max(largest_count, count)
+	return largest_count			
+
+def dfs(board, row, column, count):
+	neighbors = [(1, 0), (0, 1), (1, 1), (-1, 0), (0, -1), (-1, -1), (1, -1), (-1, 1)]
+	for neighbor in neighbors:
+		x = row + neighbor[0]
+		y = column + neighbor[1]
+		if x < 0 or y < 0 or x >= len(board) or y >= len(board[0]):
+			continue
+		if board[x][y] == 1:
+			board[row][column] = 0
+			count += 1 
+			dfs(board, x, y, count)
+	return count		
+
+# TEST					
+board = [
+			[1, 1, 0, 0],
+			[0, 1, 1, 0],
+			[0, 0, 1, 0],
+			[1, 0, 0, 0],
+		]
+print(largest_connected_component(board))
+###
+def largest_connected_component(board):
+	largest_count = 0
+	for row in range(len(board)):
+		for column in range(len(board[0])):
 			if board[row][column] == 0:
 				continue
 			else:
