1576af78b4d496505a41910f54ec9d5b23a4ac87
Jess Stewart <jeselistewart@gmail.com>
2017-03-18 09:51:18 -0700

    Update ds_trie.py

--- ds_trie.py
+++ ds_trie.py
@@ -1,46 +1,47 @@
 class TrieNode(object):
-    def __init__(self, value):
-        self.value = value
-        self.is_word = False
-        self.children = {}
-        
+  def __init__(self, letter):
+    self.letter = letter
+    self.is_word = False
+    self.children = {}
+  
 class Trie(object):
-    def __init__(self):
-        self.root = TrieNode('')
+  def __init__(self):
+    self.root = TrieNode('')
+    
+  def add(self, word):
+    node = self.root
+    
+    for letter in word:
+      if node.children.get(letter):
+        node = node.children[letter]
+      else:
+        node.children[letter] = TrieNode(letter)
+        node = node.children[letter]
         
-    def add(self, name):
-        node = self.root
+    node.is_word = True    
         
-        for letter in name:
-            if letter in node.children:
-                node = node.children[letter]
-            else:
-                node.children[letter] = TrieNode(letter)
-                node = node.children[letter]
-                
-        node.is_word = True
-        
-    def dfs(node, ):
-        if node.is_word:
-            
-        
-    def find(self, partial):
-        node = self.root
-        
-        for letter in partial:
-            if letter in node.children:
-                node = node.children[letter]
-            else: 
-                return []    
-        
-        words = []
-        word = partial
-        
-        while node.children:
-            if node.children
+  def find(self, partial):
+    node = self.root
+    
+    for letter in partial:
+      if node.children.get(letter):
+        node = node.children[letter]
+      else:
+        return []
+      
+    def dfs(node, words, word):  
+      if node.is_word:
+        words.append(word)
+      
+      for letter, child in node.children.items():
+        dfs(child, words, word+letter)
+      
+      return words  
+      
+    words = []
+    return dfs(node, words, partial)
         
 trie = Trie()
-
 n = int(input().strip())
 for a0 in range(n):
     op, contact = input().strip().split(' ')
