f0f70da4955d0af01fd0deeb8d430df131c70141
Jess Stewart <jeselistewart@gmail.com>
2017-03-20 18:32:55 -0700

    Update algo_merge_sort.py

--- algo_merge_sort.py
+++ algo_merge_sort.py
@@ -1,3 +1,42 @@
+### final ###
+def merge(left, right):
+    result = []
+    i, j, inversions = 0, 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            inversions += j
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    inversions += j*(len(left)-i)
+    result += left[i:]
+    result += right[j:]
+    return inversions, result
+
+def merge_sort(A):
+    if len(A) <= 1:
+        return 0, A
+    middle = len(A)//2
+    left_inversions, left = merge_sort(A[:middle])
+    right_inversions, right = merge_sort(A[middle:])
+    merge_inversions, merged = merge(left, right)
+    inversions = left_inversions + right_inversions + merge_inversions
+    return inversions, merged
+
+def count_inversions(arr):
+    inversions, sorted_arr = merge_sort(arr)
+    print(inversions)
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    count_inversions(arr)
+
+
+
 def merge(array, helper, start, mid, end):
 	for i in range(start, end+1):
 		helper[i] = array[i]
