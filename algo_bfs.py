cacbc6ef6747e5f02c9066829000177109f9c31c
Jess Stewart <jeselistewart@gmail.com>
2017-03-22 16:25:33 -0700

    Update algo_bfs.py

--- algo_bfs.py
+++ algo_bfs.py
@@ -1,37 +1,4 @@
-### correctly handle starting node
-'''
-undirected graph 
-distance between nodes is always 6
-s - starting node
-queries - graph, starting node
-get shortest distance from starting node to all the other nodes in the graph. Then print a single line of  space-separated integers listing node 's shortest distance to each of the  other nodes (
-ordered sequentially by node number
-if disconnected, print -1
-
-first line - number of queries
-second line - number of nodes and number of edges in the graph
-subsequent lines - edge connecting nodes 
-last line - index of starting node
-
-for each query - print space-separated integers denoting the shortest distances to each node from starting position
-listed sequentially by node number but not include node
-
-Sample Input
-2
-4 2
-1 2
-1 3
-1
-3 1
-2 3
-2
-Sample Output
-6 6 -1
--1 6
-
-DISTANCE = 6
-UNREACHABLE = -1
-'''
+### get shortest
 from collections import defaultdict
 
 class Graph(object):
@@ -82,94 +49,35 @@ class Graph(object):
 #     starting_node = int(input())
 #     graph.find_all_distances(starting_node)
 
-# g1 = Graph(4)
-# e1 = [(1, 2), (1, 3)]
-# for x, y in e1:
-# 	g1.connect(x, y)
-# g1.find_all_distances(1)	
+g1 = Graph(4)
+e1 = [(1, 2), (1, 3)]
+for x, y in e1:
+	g1.connect(x, y)
+g1.find_all_distances(1)	
 
-# g2 = Graph(3)
-# e2 = [(2, 3)]
-# for x, y in e2:
-# 	g2.connect(x, y)
-# g2.find_all_distances(2)
-
-g2 = Graph(4)
-e2 = [(2, 3), (2, 4)]
+g2 = Graph(3)
+e2 = [(2, 3)]
 for x, y in e2:
 	g2.connect(x, y)
-g2.find_all_distances(4)
+g2.find_all_distances(2)
+
+g3 = Graph(4)
+e3 = [(2, 3), (2, 4)]
+for x, y in e3:
+	g3.connect(x, y)
+g3.find_all_distances(4)
 # -1 6 12	
-    
 
-'''
-undirected graph 
-distance between nodes is always 6
-s - starting node
-queries - graph, starting node
-get shortest distance from starting node to all the other nodes in the graph. Then print a single line of  space-separated integers listing node 's shortest distance to each of the  other nodes (
-ordered sequentially by node number
-if disconnected, print -1
+g3 = Graph(4)
+e3 = [(1, 2), (2, 3), (3, 4), (4, 1)]
+for x, y in e3:
+	g3.connect(x, y)
+g3.find_all_distances(2)
+# 6 6 12
 
-first line - number of queries
-second line - number of nodes and number of edges in the graph
-subsequent lines - edge connecting nodes 
-last line - index of starting node
-
-for each query - print space-separated integers denoting the shortest distances to each node from starting position
-listed sequentially by node number but not include node
-
-Sample Input
-2
-4 2
-1 2
-1 3
-1
-3 1
-2 3
-2
-Sample Output
-6 6 -1
--1 6
-
-DISTANCE = 6
-UNREACHABLE = -1
-'''
-from collections import defaultdict
-
-class Graph(object):
-	def __init__(self, node_count):
-		self.node_count = node_count
-		self.adjacency_matrix = defaultdict(self.create_list)
-		for node in range(1, self.node_count+1):
-			self.adjacency_matrix[node_count]
-
-	@staticmethod		
-	def create_list():
-		return []		
-
-	def connect(self, x, y):
-		self.adjacency_matrix[x].append(y)
-		self.adjacency_matrix[y].append(x)
-
-	def	find_all_distances(self, starting_node):
-		result = []
-		for edge in range(1, self.node_count+1):
-			if edge == starting_node:
-				for node in self.adjacency_matrix[starting_node]:
-					result.append('6')
-			else:
-				if not self.adjacency_matrix[edge]:
-					result.append('-1')	
-
-		print(' '.join(result))	
-
-t = int(input())
-for i in range(t):
-    node_count, edge_count = [int(value) for value in input().split()]
-    graph = Graph(node_count)
-    for i in range(edge_count):
-        edge_x,edge_y = [int(edge_x) for edge_x in input().split()]
-        graph.connect(edge_x,edge_y) 
-    starting_node = int(input())
-    graph.find_all_distances(starting_node)
+g4 = Graph(5)
+e4 = [(1, 2), (1, 3), (4, 2), (4, 5), (5, 3)]
+for x, y in e4:
+	g4.connect(x, y)
+g4.find_all_distances(1)	
+# 6 6 12 12
