40c15e096393939c9cf77721c5db07fe146d3bf3
Jessica Stewart <jessica.stewart@adroll.com>
2017-03-26 08:10:17 -0700

    Update concept_bits.py

--- concept_bits.py
+++ concept_bits.py
@@ -1,3 +1,18 @@
+#!/bin/python3
+
+import sys
+
+def lonely_integer(a):
+    unique = 0
+    for num in a:
+        unique ^= num
+    return unique 
+
+n = int(input().strip())
+a = [int(a_temp) for a_temp in input().strip().split(' ')]
+print(lonely_integer(a))
+
+###
 a = [10, 10, 11, 12, 11]
 
 def lonely_integer(a):
