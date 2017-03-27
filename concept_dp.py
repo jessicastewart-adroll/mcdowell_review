afde6feb0a31d500764a4f31fac2411a87758f4e
Jess Stewart <jeselistewart@gmail.com>
2017-03-27 08:44:25 -0700

    Update concept_dp.py

--- concept_dp.py
+++ concept_dp.py
@@ -1,19 +1,16 @@
-def make_change(total, coins, cache={}):
+### recursive 
+def make_change(total, coins, count=0):
 	if total == 0:
-		return 0
-	if total == 1:
 		return 1
 
-	result = 0
-	for coin in coins:
-		if not cache.get(total-coin):
-			cache[total-coin] = make_change(total-coin, coins, cache)
-		result += cache[total-coin]
+	if total < 0:
+		return 0
 
-	return result	
+	if not coins:
+		return 0
 
-test_total = 4
-test_coins = [1, 2, 3]
-test_solution = 4
 
-print(make_change(test_total, test_coins))
+	return make_change(total-coins[0], coins, count) + make_change(total, coins[1:], count)
+
+
+print(make_change(4, [1, 2,3], count=0))
