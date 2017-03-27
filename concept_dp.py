fd8b782711c86caa056e508c1d62ebdf93e0a399
Jessica Stewart <jessica.stewart@adroll.com>
2017-03-27 15:53:44 -0700

    Update concept_dp.py

--- concept_dp.py
+++ concept_dp.py
@@ -1,4 +1,7 @@
 ### recursive 
+#!/bin/python3
+import sys
+
 def make_change(total, coins):
 	if total == 0:
 		return 1
@@ -10,5 +13,10 @@ def make_change(total, coins):
 		return 0
 
 	return make_change(total-coins[0], coins) + make_change(total, coins[1:])
+    
 
-print(make_change(4, [1, 2,3]))
+n,m = input().strip().split(' ')
+n,m = [int(n),int(m)]
+coins = [int(coins_temp) for coins_temp in input().strip().split(' ')]
+print(make_change(n, coins))
+
