42b1e082d6a43d3a81bc248a8787605b441c1395
Jess Stewart <jeselistewart@gmail.com>
2017-03-27 08:46:35 -0700

    Update concept_dp.py

--- concept_dp.py
+++ concept_dp.py
@@ -1,5 +1,5 @@
 ### recursive 
-def make_change(total, coins, count=0):
+def make_change(total, coins):
 	if total == 0:
 		return 1
 
@@ -9,6 +9,6 @@ def make_change(total, coins, count=0):
 	if not coins:
 		return 0
 
-	return make_change(total-coins[0], coins, count) + make_change(total, coins[1:], count)
+	return make_change(total-coins[0], coins) + make_change(total, coins[1:])
 
-print(make_change(4, [1, 2,3], count=0))
+print(make_change(4, [1, 2,3]))
